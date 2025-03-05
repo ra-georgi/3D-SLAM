cad52847a78bf3caa21dd5575b3c9d76f95edfe6
Reuben Georgi <reuben.abraham97@gmail.com>
2025-03-05 11:30:53 +0530

    Initial Update: 5th March

---
+++ main.py
@@ -0,0 +1,8 @@
+#main file
+
+from EKF_SLAM import EKF_SLAM
+
+#Create SLAM object
+SLAM_Object = EKF_SLAM()
+
+
---
+++ parameters.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+default_param_dict = {
+  "Initial_State": np.array([0, 0, 0, 1, 0, 0, 0]),
+  "Initial_Covariance": np.zeros(6),
+}
