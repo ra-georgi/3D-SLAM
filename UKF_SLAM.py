1533f18b6d0cf388c765930eceed7cb21d79dd43
Reuben Georgi <reuben.abraham97@gmail.com>
2025-04-09 10:42:13 +0530

    Completed first version of UKF SLAM

--- UKF_SLAM.py
+++ UKF_SLAM.py
@@ -74,15 +74,15 @@ class UKF_SLAM:
     def simulate(self):
         # Simulate Quadcopter motion as per dynamics
 
-        u_odom = np.array([0.2, 0.2, 0.2])
+        # u_odom = np.array([0.1, 0.1, 0.1])
         for i in range(self.num_time_steps-1):
 
             # Creating Odometry Data
-            # u_odom = np.array([0.2*np.sin(self.time_vector[i]), 0.2*np.cos(self.time_vector[i]), 0.2*np.sin(self.time_vector[i])])
+            u_odom = np.array([0.2*np.sin(self.time_vector[i]), 0.2*np.cos(self.time_vector[i]), 0.2*np.sin(self.time_vector[i])])
             self.robot_actual_pose[:, i+1] = self.robot_actual_pose[:, i] + u_odom
-            # u_odom[0] += 0.03*np.random.rand()
-            # u_odom[1] += 0.03*np.random.rand()
-            # u_odom[2] += 0.03*np.random.rand()
+            u_odom[0] += 0.03*np.random.rand()
+            u_odom[1] += 0.03*np.random.rand()
+            u_odom[2] += 0.03*np.random.rand()
             
             #Prediction Step
             self.mu[i+1], self.cov[i+1] = self.prediction_step(self.mu[i], self.cov[i], u_odom)
@@ -181,9 +181,9 @@ class UKF_SLAM:
                 del_z = landmark_rel_position[i,2]
                 xy_distance = np.sqrt( (del_x**2) + (del_y**2))
 
-                self.measurements[i,0] = landmark_range[i] #+ 0.03*np.random.rand()
-                self.measurements[i,1] = np.arctan2(del_z, xy_distance) #+ 0.03*np.random.rand()
-                self.measurements[i,2] = np.arctan2(del_y, del_x)  #+ 0.03*np.random.rand()
+                self.measurements[i,0] = landmark_range[i] + 0.03*np.random.rand()
+                self.measurements[i,1] = np.arctan2(del_z, xy_distance) + 0.03*np.random.rand()
+                self.measurements[i,2] = np.arctan2(del_y, del_x)  + 0.03*np.random.rand()
 
     def measurement_update(self, mu_prediction, cov_prediction):
         # Measurement incorporation step of Kalman filter
@@ -290,7 +290,144 @@ class UKF_SLAM:
 
         return expected_measurement, measurement
 
+    def animate_quad(self):
+
+        fig = plt.figure(figsize=(8,6))
+        self.ax_anim = plt.axes(projection='3d')
+        # Adjust the size of the plot within the figure
+        plt.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01)
+        self.ax2 = plt.axes([0.8, 0.7, 0.2, 0.2])
+        self.ax2.axis('off')  # Hide the axes for the inset      
+
+        # Initialize the text in the inset axes with a box around it
+        bbox_props = dict(boxstyle="round,pad=0.3", edgecolor="black", facecolor="lightgray")
+        self.text_t = self.ax2.text(0.1, 0.8, '', transform=self.ax2.transAxes, fontsize=12, bbox=bbox_props)
+        self.text_x = self.ax2.text(0.1, 0.6, '', transform=self.ax2.transAxes, fontsize=12, bbox=bbox_props)   
+        self.text_y = self.ax2.text(0.1, 0.4, '', transform=self.ax2.transAxes, fontsize=12, bbox=bbox_props)
+        self.text_z = self.ax2.text(0.1, 0.2, '', transform=self.ax2.transAxes, fontsize=12, bbox=bbox_props)                                             
+        
+        x0, y0, z0 = self.robot_actual_pose[0,0], self.robot_actual_pose[1,0], self.robot_actual_pose[2,0]
+
+        l = self.quad_length
+        self.quad_Arm1 = self.ax_anim.plot3D([x0+l, x0-l], [y0, y0], [z0, z0], lw=3 )[0]
+        self.quad_Arm2 = self.ax_anim.plot3D([x0, x0], [y0+l, y0-l], [z0, z0], lw=3 )[0]
+        self.quad_traj = self.ax_anim.plot3D(x0, y0, z0, 'gray')[0] 
+
+        #To make quadcopter's arms look equal in animation
+        self.ax_anim.set_xlim([0,10])
+        self.ax_anim.set_ylim([0,10])
+        self.ax_anim.set_zlim([0,10])      
+  
+        r = 0.1
+        #Plot Landmarks
+        u, v = np.mgrid[0:2*np.pi:7j, 0:np.pi:7j]
+        x_LM = r*np.cos(u)*np.sin(v)
+        y_LM = r*np.sin(u)*np.sin(v) 
+        z_LM = r*np.cos(v)
+        for obs in self.landmark_positions:
+                self.ax_anim.plot_surface(x_LM+obs[0], y_LM+obs[1], z_LM+obs[2], color='b',alpha=0.3)       
+        
+        # Initialize the ellipsoid surface
+        x_robot, y_robot, z_robot = self.get_ellipsoid(self.mu[0][0:3] , self.cov[0][ 0:3, 0:3])    
+        self.surf = self.ax_anim.plot_surface(x_robot, y_robot, z_robot, color='b', alpha=0.3)
+
+        self.surf_landmarks = [None] * self.num_landmarks
+        self.surf_observations = [None] * self.num_landmarks
+
+        self.ani = FuncAnimation(fig=fig, func=self.update_animation_frame,frames=self.num_time_steps, fargs=(self,),interval=50)
+        plt.show()
+
+    @staticmethod
+    def update_animation_frame(frame,self):
+            # for each frame, update the data stored on each artist.
+
+            time = self.time_vector[frame]
+            xt, yt, zt = self.robot_actual_pose[0,frame], self.robot_actual_pose[1,frame], self.robot_actual_pose[2,frame]
+
+            Q = np.eye(3)
+
+            l = self.quad_length
+
+            Arm1_Start = np.array([l,0,0])
+            Arm1_End = np.array([-l,0,0])
+
+            Arm2_Start = np.array([0,l,0])
+            Arm2_End = np.array([0,-l,0])
+
+            Arm1_Start = Q @ Arm1_Start
+            Arm1_End = Q @ Arm1_End
+            Arm2_Start = Q @ Arm2_Start
+            Arm2_End = Q @ Arm2_End              
+
+            self.quad_Arm1.set_data_3d([xt+Arm1_Start[0], xt+Arm1_End[0]], [yt+Arm1_Start[1], yt+Arm1_End[1]], [zt+Arm1_Start[2], zt+Arm1_End[2]])
+            self.quad_Arm2.set_data_3d([xt+Arm2_Start[0], xt+Arm2_End[0]], [yt+Arm2_Start[1], yt+Arm2_End[1]], [zt+Arm2_Start[2], zt+Arm2_End[2]])
+            self.quad_traj.set_data_3d(self.robot_actual_pose[0,:frame],self.robot_actual_pose[1,:frame],self.robot_actual_pose[2,:frame])
+
+            self.text_t.set_text(f't = {time:.2f} s')
+            self.text_x.set_text(f'x = {xt:.2f} m')
+            self.text_y.set_text(f'y = {yt:.2f} m')
+            self.text_z.set_text(f'z = {zt:.2f} m')
+
+            self.surf.remove()  # Remove the old surface
+            x_robot, y_robot, z_robot = self.get_ellipsoid(self.mu[frame][0:3], self.cov[frame][0:3, 0:3])    
+            self.surf = self.ax_anim.plot_surface(x_robot, y_robot, z_robot, color='b', alpha=0.2)
+
+            for i in range(self.num_landmarks):
+                if self.surf_landmarks[i] is not None:
+                    temp_surf = self.surf_landmarks[i]
+                    temp_surf.remove()  # Initial covariance is set to a high value, so will cover entire plot  
+
+                landmark_rel_order = int(self.measurements[i, 5])
+                dim_ss = self.mu[frame].shape[0]
+                landmark_rel_index = self.robot_state_size + (3*landmark_rel_order)
+                if dim_ss > (landmark_rel_index + 1):
+                    # Check covariance size (using max eigenvalue)
+                    # landmark_start_index = self.robot_state_size + (3*i)
+                    eigvals = np.linalg.eigh(self.cov[frame][landmark_rel_index : landmark_rel_index + 3, landmark_rel_index : landmark_rel_index + 3])[0]  # Only need eigenvalues
+                    if np.max(eigvals) < 1000:  # Plot only if covariance is not too large
+                        x_LM, y_LM, z_LM = self.get_ellipsoid(
+                                self.mu[frame][landmark_rel_index : landmark_rel_index + 3], 
+                                self.cov[frame][landmark_rel_index : landmark_rel_index + 3, landmark_rel_index : landmark_rel_index + 3]
+                                )
+                        self.surf_landmarks[i] = self.ax_anim.plot_surface(x_LM, y_LM, z_LM , color='r', alpha=0.3)   
+                    else:
+                        self.surf_landmarks[i] = None
+
+            for i in range(self.num_landmarks):
+                landmark_rel_position =  1e5*np.ones((self.num_landmarks,3))
+                landmark_range        =  1e5*np.ones((self.num_landmarks))
+                if self.surf_observations[i] is not None:
+                    temp_surf = self.surf_observations[i]
+                    temp_surf.remove()  
+                landmark_rel_position[i,:] = self.landmark_positions[i, :] - self.mu[frame][0:3]
+                landmark_range[i] = np.sqrt( np.dot(landmark_rel_position[i,:],landmark_rel_position[i,:]) )
+                if landmark_range[i] < self.sensor_range:
+                    self.surf_observations[i] = self.ax_anim.plot3D(
+                         [self.mu[frame][0], self.landmark_positions[i, 0]], 
+                         [self.mu[frame][1], self.landmark_positions[i, 1]], 
+                         [self.mu[frame][2], self.landmark_positions[i, 2]], lw=3 )[0]
+                else:                 
+                    self.surf_observations[i] = None
+
+            
+            return 
 
 
-
-
+    def get_ellipsoid(self, mean, cov, scale=2.45):  # scale=2.45 for ~95% confidence
+         
+        # Function to generate ellipsoid points
+        u = np.linspace(0, 2 * np.pi, 15)  # Reduced resolution for speed
+        v = np.linspace(0, np.pi, 15)
+        x = np.outer(np.cos(u), np.sin(v))
+        y = np.outer(np.sin(u), np.sin(v))
+        z = np.outer(np.ones(np.size(u)), np.cos(v))
+        sphere = np.stack([x, y, z], axis=0) * scale
+        
+        eigvals, eigvecs = np.linalg.eigh(cov)
+        radii = np.sqrt(np.maximum(eigvals, 0))  # Ensure non-negative
+        transform = eigvecs @ np.diag(radii)
+        
+        ellipsoid = transform @ sphere.reshape(3, -1) + mean[:, np.newaxis]
+        return (ellipsoid[0].reshape(x.shape),
+                ellipsoid[1].reshape(y.shape),
+                ellipsoid[2].reshape(z.shape))    
